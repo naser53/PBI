aa62b5c75b6bbe6f3615a2dc06a861341e074f40
Naser Daneshi <naser.daneshi@entaingroup.com>
2026-09-22 16:23:26 +0200

    This first push
    This visual displays a slider or Button selector as a ramge selecteor

---
+++ ButtonSlider/.tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var buttonSliderF1A2B3C4D5E6F7A8B9C0D1E2F3A4B5C6: IVisualPlugin;
+export default buttonSliderF1A2B3C4D5E6F7A8B9C0D1E2F3A4B5C6;
